333430cad6bda38fe4c602333f03c8874e17edf6
Aaron <37417949+brotheraaron@users.noreply.github.com>
2021-01-14 12:46:31 -0800

    Create provider.tf

---
+++ terraform/OKEClusterUsingTerraform/provider.tf
@@ -0,0 +1,9 @@
+# https://www.terraform.io/docs/configuration/providers.html
+# https://www.terraform.io/docs/providers/oci/index.html
+provider "oci" {
+  tenancy_ocid     = "${var.tenancy_ocid}"
+  user_ocid        = "${var.user_ocid}"
+  fingerprint      = "${var.fingerprint}"
+  private_key_path = "${var.private_key_path}"
+  region           = "${var.region}"
+}
